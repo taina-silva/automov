473242498cc32249b9b8eeedbc800898bd100971
Tainá Santos <tainassluz@gmail.com>
2024-09-20 10:04:00 -0300

    páginas de Login e Cadastro

--- script.js
+++ script.js
@@ -25,5 +25,5 @@ function generateStaticVehicleCards(numCards) {
 
 const loginButton = document.getElementById("go-to-login-button");
 loginButton.addEventListener("click", function () {
-  window.location.href='login.html';
+  window.location.href='./pages/login.html';
 });
