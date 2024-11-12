540131c1b98b05b697da1a66431d7292cfc4b7b0
Tainá Santos <tainassluz@gmail.com>
2024-11-12 03:44:53 -0300

    atualizando conexao sql

--- php/conexao_sql.php
+++ php/conexao_sql.php
@@ -2,10 +2,15 @@
 
 function conectaSql()
 {
-  $db_host = "localhost";
-  $db_username = "admin";
-  $db_password = "123123";
-  $db_name = "automov";
+  // $db_host = "localhost";
+  // $db_username = "admin";
+  // $db_password = "123123";
+  // $db_name = "automov";
+
+  $db_host = "sql110.infinityfree.com";
+  $db_username = "if0_37101055";
+  $db_password = "YZhi3brj8q1foY";
+  $db_name = "if0_37101055_ppi";
 
   $options = [
     PDO::ATTR_EMULATE_PREPARES => false,
